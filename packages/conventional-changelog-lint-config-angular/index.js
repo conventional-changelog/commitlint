6afa7fdae29793a343e973bb10f309666bcf5d0a
Mario Nebl <hello@herebecode.com>
2016-02-14 14:33:21 +0100

    style: remove unneeded disabled rules

--- packages/conventional-changelog-lint-config-angular/index.js
+++ packages/conventional-changelog-lint-config-angular/index.js
@@ -20,77 +20,23 @@ module.exports = {
 		'type-empty': [2,
 			'never'
 		],
-		'type-max-length': [0,
-			'always',
-			'Infinity'
-		],
-		'type-min-length': [0,
-			'always',
-			0
-		],
-		'scope-enum': [0,
-			'always',
-			[]
-		],
 		'scope-case': [2,
-			'always',
-			'lowerCase'
-		],
-		'scope-empty': [0,
-			'never'
-		],
-		'scope-max-length': [0,
-			'always',
-			'Infinity'
-		],
-		'scope-min-length': [0,
-			'always',
-			0
-		],
-		'subject-case': [0,
 			'always',
 			'lowerCase'
 		],
 		'subject-empty': [2,
 			'never'
 		],
-		'subject-max-length': [0,
-			'always',
-			'Infinity'
-		],
-		'subject-min-length': [0,
-			'always',
-			0
-		],
 		'subject-full-stop': [2,
 			'never',
 			'.'
 		],
-		'body-case': [0,
-			'always',
-			'lowerCase'
-		],
-		'body-empty': [0,
-			'never'
-		],
 		'body-leading-blank': [1,
 			'always'
-		],
-		'body-max-length': [0,
-			'always',
-			'Infinity'
-		],
-		'body-min-length': [0,
-			'always',
-			0
 		],
 		'header-max-length': [2,
 			'always',
 			72
-		],
-		'header-min-length': [0,
-			'always',
-			0
 		],
 		'lang': [1,
 			'always',
