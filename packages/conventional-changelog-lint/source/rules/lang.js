11cba39d61b8c723ea58c85efa3336f0f395d048
Mario Nebl <hello@herebecode.com>
2016-02-14 13:25:02 +0100

    fix: be less strict with matched languages

--- packages/conventional-changelog-lint/source/rules/lang.js
+++ packages/conventional-changelog-lint/source/rules/lang.js
@@ -2,14 +2,17 @@ import franc from 'franc';
 
 export default (parsed, when, value) => {
 	const negated = when === 'never';
-	const detected = franc.all(parsed.subject);
-	const matches = Object.keys(detected).indexOf(value) > -1;
+	const detected = franc.all(parsed.subject)
+		.filter(lang => lang[1] >= 0.45)
+		.map(lang => lang[0]);
+	const matches = detected.indexOf(value) > -1;
+
 	return [
 		negated ? !matches : matches,
 		[
 			'commit',
 			negated ? 'may not' : 'must',
-			`be in languague ${value}`
+			`be in languague "${value}", was one of: ${detected.join(', ')}`
 		]
 		.filter(Boolean)
 		.join(' ')
