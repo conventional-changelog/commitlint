03aa9b24d3c2f2826b4003c1b9524b73b0c45b91
Mario Nebl <hello@herebecode.com>
2016-02-22 14:22:27 +0100

    test: foo
    fooo

--- packages/conventional-changelog-lint/source/rules/body-leading-blank.js
+++ packages/conventional-changelog-lint/source/rules/body-leading-blank.js
@@ -1,10 +1,13 @@
 export default (parsed, when) => {
 	const negated = when === 'never';
 	const lines = (parsed.body || '').split('\n');
+	console.log(parsed);
+	console.log(lines);
 	const leadingBlank =
 		lines.length > 0 ?
 		lines[0].length === 0 :
 		true;
+	console.log(leadingBlank);
 	return [
 		negated ? !leadingBlank : leadingBlank,
 		[
