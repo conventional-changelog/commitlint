48485f22eb8b09fb85f4f87efe47dd6c829f61d5
Mario Nebl <hello@herebecode.com>
2016-02-22 14:29:37 +0100

    fix: use raw commit message to check for leading blank line

--- packages/conventional-changelog-lint/source/rules/body-leading-blank.js
+++ packages/conventional-changelog-lint/source/rules/body-leading-blank.js
@@ -1,13 +1,12 @@
 export default (parsed, when) => {
 	const negated = when === 'never';
-	const lines = (parsed.body || '').split('\n');
-	console.log(parsed);
-	console.log(lines);
+	// get complete body split into lines
+	const lines = (parsed.raw || '').split('\n').slice(1);
+	// check if the first line of body (if any) is empty
 	const leadingBlank =
 		lines.length > 0 ?
 		lines[0].length === 0 :
 		true;
-	console.log(leadingBlank);
 	return [
 		negated ? !leadingBlank : leadingBlank,
 		[
