0abf2c38bcc8130e2d145154ab7f64fe9950875d
Mario Nebl <hello@herebecode.com>
2016-02-25 10:39:00 +0100

    feat: extend config-angular with rule for scope-enum

---
+++ packages/conventional-changelog-lint-config-patternplate/index.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const globby = require('globby');
+const merge = require('lodash').merge;
+
+function pathToId(root, filePath) {
+	const relativePath = path.relative(root, filePath);
+	return path.dirname(relativePath).split(path.sep).join('/');
+}
+
+function getPatternIDs() {
+	const root = path.resolve(process.cwd(), './patterns');
+	const glob = path.resolve(root, '**/pattern.json');
+	return globby(glob)
+		.then(results => results.map(result => pathToId(root, result)));
+}
+
+module.exports = merge(
+	require('conventional-changelog-lint-config-angular'),
+	{
+		rules: {
+			'scope-enum': () => getPatternIDs()
+				.then(ids => [2, 'always', ids.concat(['system'])])
+		}
+	}
+);
