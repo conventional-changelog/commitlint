bb17ab9361ab861d37b8538b14b4dce58312ebef
Mario Nebl <hello@herebecode.com>
2016-02-25 12:23:01 +0100

    refactor: move lang detection to library module

---
+++ source/library/ensure-language.js
@@ -0,0 +1,17 @@
+import franc from 'franc';
+
+export default (input, allowed) => {
+	const detected = franc.all(input)
+		.filter(lang => lang[1] >= 0.45)
+		.map(lang => lang[0]);
+
+	// franc spits out ['und'] when unable to
+	// guess any languages, let it through in this case
+	const matches = detected[0] === 'und' ||
+		detected.indexOf(allowed) > -1;
+
+	return {
+		matches,
+		detected
+	};
+};
