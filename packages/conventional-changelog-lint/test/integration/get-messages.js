a351955769c6a00055b73d705ce19e6fd40739db
Mario Nebl <hello@herebecode.com>
2017-03-12 18:37:31 +0100

    fix: determine git toplevel before reading .git/COMMIT_EDITMSG
    test: set git user and email
    
    test: ensure git ident setting do not bleed
    
    test: rm temporary dir from outside to ensure windows compat

--- packages/conventional-changelog-lint/test/integration/get-messages.js
+++ packages/conventional-changelog-lint/test/integration/get-messages.js
@@ -5,7 +5,7 @@ import {join} from 'path';
 import test from 'ava';
 import denodeify from 'denodeify';
 import execa from 'execa';
-import {writeFile} from 'mz/fs';
+import {mkdir, writeFile} from 'mz/fs';
 import exists from 'path-exists';
 import rimraf from 'rimraf';
 import expect from 'unexpected';
@@ -44,19 +44,42 @@ test.serial('get history commit messages', async () => {
 	await cleanRepository(repo);
 });
 
+test.serial('get edit commit message from git subdirectory', async () => {
+	const repo = await initRepository();
+
+	await mkdir('beta');
+	await writeFile('beta/beta.txt', 'beta');
+	process.chdir('beta');
+	await execa('git', ['add', '.']);
+	await execa('git', ['commit', '-m', 'beta']);
+
+	const expected = ['beta\n\n'];
+	const actual = await getMessages({edit: true});
+	expect(actual, 'to equal', expected);
+
+	await cleanRepository(repo);
+});
+
 async function initRepository() {
 	const previous = process.cwd();
 	const directory = join(tmpdir(), rand());
+
 	await execa('git', ['init', directory]);
+
 	process.chdir(directory);
+
+	await execa('git', ['config', 'user.email', 'test@example.com']);
+	await execa('git', ['config', 'user.name', 'ava']);
+
 	return {directory, previous};
 }
 
 async function cleanRepository(repo) {
+	process.chdir(repo.previous);
+
 	if (await exists(repo.directory)) {
 		await rm(repo.directory);
 	}
-	process.chdir(repo.previous);
 }
 
 function rand() {
