380705b4a491b40b928d3291346c2ad6ccddb3af
Mario Nebl <hello@herebecode.com>
2017-05-01 17:50:05 +0200

    test: correct non-criticals in footer-max-length tests

--- test/rules/footer-max-length.js
+++ test/rules/footer-max-length.js
@@ -5,6 +5,8 @@ import footerMaxLength from '../../source/rules/footer-max-length';
 const short = 'BREAKING CHANGE: a';
 const long = 'BREAKING CHANGE: ab';
 
+const allowed = short.length;
+
 const messages = {
 	simple: 'chore: subject',
 	empty: 'chore: subject\nbody',
@@ -16,30 +18,30 @@ const parsed = {
 	simple: parse(messages.simple),
 	empty: parse(messages.empty),
 	short: parse(messages.short),
-	long: parse(messages.short)
+	long: parse(messages.long)
 };
 
 test('footer-max-length with simple message should succeed', t => {
-	const [actual] = footerMaxLength(parsed.simple, '', short.length);
+	const [actual] = footerMaxLength(parsed.simple, '', allowed);
 	const expected = true;
 	t.is(actual, expected);
 });
 
 test('footer-max-length with empty footer should succeed', t => {
-	const [actual] = footerMaxLength(parsed.empty, '', short.length);
+	const [actual] = footerMaxLength(parsed.empty, '', allowed);
 	const expected = true;
 	t.is(actual, expected);
 });
 
 test('footer-max-length with short footer should succeed', t => {
-	const [actual] = footerMaxLength(parsed.short, '', short.length);
+	const [actual] = footerMaxLength(parsed.short, '', allowed);
 	const expected = true;
 	t.is(actual, expected);
 });
 
-test('footer-max-length with long footer should succeed', t => {
-	const [actual] = footerMaxLength(parsed.long, '', short.length);
-	const expected = true;
+test('footer-max-length with long footer should fail', t => {
+	const [actual] = footerMaxLength(parsed.long, '', allowed);
+	const expected = false;
 	t.is(actual, expected);
 });
 
