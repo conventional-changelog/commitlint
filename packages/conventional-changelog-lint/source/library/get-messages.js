0a568d6658e4027bed159cf3f1f8d950620f72f0
Mario Nebl <marionebl@users.noreply.github.com>
2017-05-01 16:57:49 +0200

    fix: throw when detecting a shallow clone
    * fix: warn about shallow clones
    
    * docs: help with shallow clone troubleshooting
    
    * closes #7
    * closes #12
    
    * test: simplify repo setup and teardown

--- packages/conventional-changelog-lint/source/library/get-messages.js
+++ packages/conventional-changelog-lint/source/library/get-messages.js
@@ -1,9 +1,16 @@
 import {join} from 'path';
+import exists from 'path-exists';
 import gitRawCommits from 'git-raw-commits';
 import gitToplevel from 'git-toplevel';
 import {readFile} from 'mz/fs';
 
 export default getCommitMessages;
+
+const SHALLOW_MESSAGE = [
+	'Could not get git history from shallow clone.',
+	'Use git fetch --shallow before linting.',
+	'Original issue: https://git.io/vyKMq\n Refer to https://git.io/vyKMv for details.'
+].join('\n');
 
 // Get commit messages
 // Object => Promise<Array<String>>
@@ -12,6 +19,10 @@ async function getCommitMessages(settings) {
 
 	if (edit) {
 		return getEditCommit();
+	}
+
+	if (await isShallow()) {
+		throw new Error(SHALLOW_MESSAGE);
 	}
 
 	return await getHistoryCommits({from, to});
@@ -29,6 +40,14 @@ function getHistoryCommits(options) {
 			resolve(data);
 		});
 	});
+}
+
+// Check if the current repository is shallow
+// () => Promise<Boolean>
+async function isShallow() {
+	const top = await gitToplevel();
+	const shallow = join(top, '.git/shallow');
+	return await exists(shallow);
 }
 
 // Get recently edited commit message
