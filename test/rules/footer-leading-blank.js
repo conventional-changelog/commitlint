05b4427f64ae23b56b2a75ddb689ef377492e3bc
Mario Nebl <marionebl@users.noreply.github.com>
2017-05-01 17:26:16 +0200

    fix: prevent false positives for footer-leading-blank (#33)
    * test: add test cases for footer-leading-blank
    
    * fix: succeed footer-leading-blank if no footer is found
    
    * fix: sanitize check for leading blank line before footer

---
+++ test/rules/footer-leading-blank.js
@@ -0,0 +1,109 @@
+import test from 'ava';
+import parse from '../../source/library/parse';
+import footerLeadingBlank from '../../source/rules/footer-leading-blank';
+
+const messages = {
+	simple: 'chore: subject',
+	body: 'chore: subject\nbody',
+	trailing: 'chore: subject\nbody\n\n',
+	without: 'chore: subject\nbody\nBREAKING CHANGE: something important',
+	with: 'chore: subject\nbody\n\nBREAKING CHANGE: something important'
+};
+
+const parsed = {
+	simple: parse(messages.simple),
+	body: parse(messages.body),
+	trailing: parse(messages.trailing),
+	without: parse(messages.without),
+	with: parse(messages.with)
+};
+
+test('footer-leading-blank with simple message should succeed for empty keyword', t => {
+	const [actual] = footerLeadingBlank(parsed.simple);
+	const expected = true;
+	t.is(actual, expected);
+});
+
+test('footer-leading-blank with simple message should succeed for "never"', t => {
+	const [actual] = footerLeadingBlank(parsed.simple, 'never');
+	const expected = true;
+	t.is(actual, expected);
+});
+
+test('footer-leading-blank with simple message should succeed for "always"', t => {
+	const [actual] = footerLeadingBlank(parsed.simple, 'always');
+	const expected = true;
+	t.is(actual, expected);
+});
+
+test('footer-leading-blank with body message should succeed for empty keyword', t => {
+	const [actual] = footerLeadingBlank(parsed.body);
+	const expected = true;
+	t.is(actual, expected);
+});
+
+test('footer-leading-blank with body message should succeed for "never"', t => {
+	const [actual] = footerLeadingBlank(parsed.body, 'never');
+	const expected = true;
+	t.is(actual, expected);
+});
+
+test('footer-leading-blank with body message should succeed for "always"', t => {
+	const [actual] = footerLeadingBlank(parsed.body, 'always');
+	const expected = true;
+	t.is(actual, expected);
+});
+
+test('footer-leading-blank with trailing message should succeed for empty keyword', t => {
+	const [actual] = footerLeadingBlank(parsed.trailing);
+	const expected = true;
+	t.is(actual, expected);
+});
+
+test('footer-leading-blank with trailing message should succeed for "never"', t => {
+	const [actual] = footerLeadingBlank(parsed.trailing, 'never');
+	const expected = true;
+	t.is(actual, expected);
+});
+
+test('footer-leading-blank with trailing message should succeed for "always"', t => {
+	const [actual] = footerLeadingBlank(parsed.trailing, 'always');
+	const expected = true;
+	t.is(actual, expected);
+});
+
+test('footer-leading-blank without blank line before footer should fail for empty keyword', t => {
+	const [actual] = footerLeadingBlank(parsed.without);
+	const expected = false;
+	t.is(actual, expected);
+});
+
+test('footer-leading-blank without blank line before footer should succeed for "never"', t => {
+	const [actual] = footerLeadingBlank(parsed.without, 'never');
+	const expected = true;
+	t.is(actual, expected);
+});
+
+test('footer-leading-blank without blank line before footer should fail for "always"', t => {
+	const [actual] = footerLeadingBlank(parsed.without, 'always');
+	const expected = false;
+	t.is(actual, expected);
+});
+
+test('footer-leading-blank with blank line before footer should succeed for empty keyword', t => {
+	const [actual] = footerLeadingBlank(parsed.with);
+	const expected = true;
+	t.is(actual, expected);
+});
+
+test('footer-leading-blank with blank line before footer should fail for "never"', t => {
+	const [actual] = footerLeadingBlank(parsed.with, 'never');
+	const expected = false;
+	t.is(actual, expected);
+});
+
+test('footer-leading-blank with blank line before footer should succeed for "always"', t => {
+	const [actual] = footerLeadingBlank(parsed.with, 'always');
+	const expected = true;
+	t.is(actual, expected);
+});
