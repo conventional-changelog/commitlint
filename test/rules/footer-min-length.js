bd1e874443c3f92e5e1374b161aebcd026155bb6
Mario Nebl <hello@herebecode.com>
2017-05-01 17:57:48 +0200

    test: add test cases for footer-min-length

---
+++ test/rules/footer-min-length.js
@@ -0,0 +1,47 @@
+import test from 'ava';
+import parse from '../../source/library/parse';
+import footerMinLength from '../../source/rules/footer-min-length';
+
+const short = 'BREAKING CHANGE: a';
+const long = 'BREAKING CHANGE: ab';
+
+const needed = long.length;
+
+const messages = {
+	simple: 'chore: subject',
+	empty: 'chore: subject\nbody',
+	short: `chore: subject\n${short}`,
+	long: `chore: subject\n${long}`
+};
+
+const parsed = {
+	simple: parse(messages.simple),
+	empty: parse(messages.empty),
+	short: parse(messages.short),
+	long: parse(messages.long)
+};
+
+test.failing('footer-min-length with simple message should succeed', t => {
+	const [actual] = footerMinLength(parsed.simple, '', needed);
+	const expected = true;
+	t.is(actual, expected);
+});
+
+test.failing('footer-min-length with empty footer should succeed', t => {
+	const [actual] = footerMinLength(parsed.empty, '', needed);
+	const expected = true;
+	t.is(actual, expected);
+});
+
+test('footer-min-length with short footer should fail', t => {
+	const [actual] = footerMinLength(parsed.short, '', needed);
+	const expected = false;
+	t.is(actual, expected);
+});
+
+test('footer-min-length with long footer should succeed', t => {
+	const [actual] = footerMinLength(parsed.long, '', needed);
+	const expected = true;
+	t.is(actual, expected);
+});
+
