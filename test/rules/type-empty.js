6194f4a72afc83a3b85c5f4081457bcaf8b68971
Mario Nebl <hello@herebecode.com>
2017-05-01 23:19:36 +0200

    test: add cases for type-empty

---
+++ test/rules/type-empty.js
@@ -0,0 +1,49 @@
+import test from 'ava';
+import parse from '../../source/library/parse';
+import typeEmpty from '../../source/rules/type-empty';
+
+const messages = {
+	empty: '(scope):',
+	filled: 'type: subject'
+};
+
+const parsed = {
+	empty: parse(messages.empty),
+	filled: parse(messages.filled)
+};
+
+test.failing('without type should succeed for empty keyword', t => {
+	const [actual] = typeEmpty(parsed.empty);
+	const expected = true;
+	t.is(actual, expected);
+});
+
+test('without type should fail for "never"', t => {
+	const [actual] = typeEmpty(parsed.empty, 'never');
+	const expected = false;
+	t.is(actual, expected);
+});
+
+test.failing('without type should succeed for "always"', t => {
+	const [actual] = typeEmpty(parsed.empty, 'always');
+	const expected = true;
+	t.is(actual, expected);
+});
+
+test.failing('with type fail for empty keyword', t => {
+	const [actual] = typeEmpty(parsed.filled);
+	const expected = false;
+	t.is(actual, expected);
+});
+
+test('with type succeed for "never"', t => {
+	const [actual] = typeEmpty(parsed.filled, 'never');
+	const expected = true;
+	t.is(actual, expected);
+});
+
+test.failing('with type fail for "always"', t => {
+	const [actual] = typeEmpty(parsed.filled, 'always');
+	const expected = false;
+	t.is(actual, expected);
+});
