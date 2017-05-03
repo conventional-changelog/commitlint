9dd236ae85d71fc2dc0850f39e80d810b103741f
Mario Nebl <hello@herebecode.com>
2017-05-03 09:43:38 +0200

    chore: enable clone pr linting

---
+++ scripts/lint-prcommits.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+set -u
+
+# Add the clone as remote if this is a PR from a clone
+if [ $TRAVIS_PULL_REQUEST_SLUG != "" ] && [ $TRAVIS_PULL_REQUEST_SLUG != $TRAVIS_REPO_SLUG ]; then
+	git remote add "$TRAVIS_PULL_REQUEST_SLUG" "https://github.com/$TRAVIS_PULL_REQUEST_SLUG.git"
+	git fetch "$TRAVIS_PULL_REQUEST_SLUG"
+fi
+
+# Use REMOTE/BRANCH as comparison if applicable
+if [ $TRAVIS_PULL_REQUEST_SLUG != "" ]; then
+	TO="$TRAVIS_PULL_REQUEST_SLUG/$TRAVIS_PULL_REQUEST_BRANCH"
+else
+	TO="$TRAVIS_PULL_REQUEST_BRANCH"
+fi
+
+conventional-changelog-lint --from="$TRAVIS_BRANCH" --to="$TO"
