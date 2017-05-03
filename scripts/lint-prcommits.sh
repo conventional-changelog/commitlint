ac820c8d06cddcd18685f15d6e059fbe95768689
Mario Nebl <hello@herebecode.com>
2017-05-03 10:03:04 +0200

    chore: solidify lint-prcommits

--- scripts/lint-prcommits.sh
+++ scripts/lint-prcommits.sh
@@ -3,16 +3,20 @@ set -e
 set -u
 
 # Add the clone as remote if this is a PR from a clone
-if [ $TRAVIS_PULL_REQUEST_SLUG != "" ] && [ $TRAVIS_PULL_REQUEST_SLUG != $TRAVIS_REPO_SLUG ]; then
+if [[ $TRAVIS_PULL_REQUEST_SLUG != "" && $TRAVIS_PULL_REQUEST_SLUG != $TRAVIS_REPO_SLUG ]]; then
 	git remote add "$TRAVIS_PULL_REQUEST_SLUG" "https://github.com/$TRAVIS_PULL_REQUEST_SLUG.git"
 	git fetch "$TRAVIS_PULL_REQUEST_SLUG"
 fi
 
 # Use REMOTE/BRANCH as comparison if applicable
-if [ $TRAVIS_PULL_REQUEST_SLUG != "" ]; then
+if [[ $TRAVIS_PULL_REQUEST_SLUG != "" ]]; then
 	TO="$TRAVIS_PULL_REQUEST_SLUG/$TRAVIS_PULL_REQUEST_BRANCH"
 else
 	TO="$TRAVIS_PULL_REQUEST_BRANCH"
 fi
 
-conventional-changelog-lint --from="$TRAVIS_BRANCH" --to="$TO"
+if [[ $TRAVIS_BRANCH == $TO ]]; then
+	conventional-changelog-lint --from="HEAD~1"
+else
+	conventional-changelog-lint --from="$TRAVIS_BRANCH" --to="$TO"
+fi
