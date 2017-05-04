bb3fe0d001e999f4d4b5a926ad026b45a266ff37
Mario Nebl <hello@herebecode.com>
2017-05-04 16:05:19 +0200

    fix: handle empty scope

--- source/rules/scope-case.js
+++ source/rules/scope-case.js
@@ -1,7 +1,14 @@
 import ensureCase from '../library/ensure-case';
 
 export default (parsed, when, value) => {
+	const {scope} = parsed;
+
+	if (!scope) {
+		return [true];
+	}
+
 	const negated = when === 'never';
+
 	const result = ensureCase(parsed.scope, value);
 	return [
 		negated ? !result : result,
