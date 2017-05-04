239b7a02f18494d475800fd424115134c76f9795
Mario Nebl <hello@herebecode.com>
2017-05-04 14:31:48 +0200

    test: add body-min-length cases

---
+++ source/rules/body-min-length.test.js
@@ -0,0 +1,38 @@
+import test from 'ava';
+import parse from '../library/parse';
+import bodyMinLength from './body-min-length';
+
+const short = 'a';
+const long = 'ab';
+
+const needed = long.length;
+
+const messages = {
+	simple: 'chore: subject',
+	short: `chore: subject\n${short}`,
+	long: `chore: subject\n${long}`
+};
+
+const parsed = {
+	simple: parse(messages.simple),
+	short: parse(messages.short),
+	long: parse(messages.long)
+};
+
+test.failing('with simple message should succeed', t => {
+	const [actual] = bodyMinLength(parsed.simple, '', needed);
+	const expected = true;
+	t.is(actual, expected);
+});
+
+test('with short body should fail', t => {
+	const [actual] = bodyMinLength(parsed.short, '', needed);
+	const expected = false;
+	t.is(actual, expected);
+});
+
+test('with long body should succeed', t => {
+	const [actual] = bodyMinLength(parsed.long, '', needed);
+	const expected = true;
+	t.is(actual, expected);
+});
