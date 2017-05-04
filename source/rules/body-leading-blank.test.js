1fdbdae5d921d872cae818b6de01420b08855882
Mario Nebl <hello@herebecode.com>
2017-05-04 14:08:59 +0200

    test: add cases for body-leading-blank

---
+++ source/rules/body-leading-blank.test.js
@@ -0,0 +1,69 @@
+import test from 'ava';
+import parse from '../library/parse';
+import bodyLeadingBlank from './body-leading-blank';
+
+const messages = {
+	simple: 'chore: subject',
+	without: 'chore: subject\nbody',
+	with: 'chore: subject\n\nbody'
+};
+
+const parsed = {
+	simple: parse(messages.simple),
+	without: parse(messages.without),
+	with: parse(messages.with)
+};
+
+test('with simple message should succeed for empty keyword', t => {
+	const [actual] = bodyLeadingBlank(parsed.simple);
+	const expected = true;
+	t.is(actual, expected);
+});
+
+test.failing('with simple message should succeed for "never"', t => {
+	const [actual] = bodyLeadingBlank(parsed.simple, 'never');
+	const expected = true;
+	t.is(actual, expected);
+});
+
+test('with simple message should succeed for "always"', t => {
+	const [actual] = bodyLeadingBlank(parsed.simple, 'always');
+	const expected = true;
+	t.is(actual, expected);
+});
+
+test('without blank line before body should fail for empty keyword', t => {
+	const [actual] = bodyLeadingBlank(parsed.without);
+	const expected = false;
+	t.is(actual, expected);
+});
+
+test('without blank line before body should succeed for "never"', t => {
+	const [actual] = bodyLeadingBlank(parsed.without, 'never');
+	const expected = true;
+	t.is(actual, expected);
+});
+
+test('without blank line before body should fail for "always"', t => {
+	const [actual] = bodyLeadingBlank(parsed.without, 'always');
+	const expected = false;
+	t.is(actual, expected);
+});
+
+test('with blank line before body should succeed for empty keyword', t => {
+	const [actual] = bodyLeadingBlank(parsed.with);
+	const expected = true;
+	t.is(actual, expected);
+});
+
+test('with blank line before body should fail for "never"', t => {
+	const [actual] = bodyLeadingBlank(parsed.with, 'never');
+	const expected = false;
+	t.is(actual, expected);
+});
+
+test('with blank line before body should succeed for "always"', t => {
+	const [actual] = bodyLeadingBlank(parsed.with, 'always');
+	const expected = true;
+	t.is(actual, expected);
+});
