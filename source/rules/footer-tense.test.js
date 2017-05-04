6113353ce3b363613f4cb74f1a8d5249e1c66bd4
Mario Nebl <hello@herebecode.com>
2017-05-04 15:44:56 +0200

    feat: introduce object based config for footer-tense

--- source/rules/footer-tense.test.js
+++ source/rules/footer-tense.test.js
@@ -6,8 +6,9 @@ const messages = {
 	empty: 'chore: subject\nbody',
 	presentImperative: `chore: subject\nBREAKING CHANGE: we implement things`,
 	presentParticiple: `chore: subject\nBREAKING CHANGE: implementing things`,
-	presentThirdPerson: `chore: implements things`,
-	past: `chore: subject\nBREAKING CHANGE: we did implement things`
+	presentThirdPerson: `chore: subject\nBREAKING CHANGE: implements things`,
+	past: `chore: subject\nBREAKING CHANGE: we did implement things`,
+	mixed: `chore: subject\nBREAKING CHANGE: implement, implementing, implements, implemented`
 };
 
 const parsed = {
@@ -15,7 +16,8 @@ const parsed = {
 	presentImperative: parse(messages.presentImperative),
 	presentParticiple: parse(messages.presentParticiple),
 	presentThirdPerson: parse(messages.presentImperative),
-	past: parse(messages.past)
+	past: parse(messages.past),
+	mixed: parse(messages.mixed)
 };
 
 test('with empty footer should succeed', t => {
@@ -24,13 +26,13 @@ test('with empty footer should succeed', t => {
 	t.is(actual, expected);
 });
 
-test.failing('with present footer should succeed for "always present-imperative"', t => {
+test('with present footer should succeed for "always present-imperative"', t => {
 	const [actual] = footerTense(parsed.presentImperative, 'always', ['present-imperative']);
 	const expected = true;
 	t.is(actual, expected);
 });
 
-test.failing('with present footer should fail for "never present-imperative"', t => {
+test('with present footer should fail for "never present-imperative"', t => {
 	const [actual] = footerTense(parsed.presentImperative, 'never', ['present-imperative']);
 	const expected = false;
 	t.is(actual, expected);
@@ -48,26 +50,65 @@ test('with present footer should fail for "never present-participle"', t => {
 	t.is(actual, expected);
 });
 
-test.failing('with present footer should succeed for "always present-third-person"', t => {
+test('with present footer should succeed for "always present-third-person"', t => {
 	const [actual] = footerTense(parsed.presentThirdPerson, 'always', ['present-third-person']);
 	const expected = true;
 	t.is(actual, expected);
 });
 
-test.failing('with present footer should fail for "never present-third-person"', t => {
+test('with present footer should fail for "never present-third-person"', t => {
 	const [actual] = footerTense(parsed.presentThirdPerson, 'never', ['present-third-person']);
 	const expected = false;
 	t.is(actual, expected);
 });
 
-test.failing('with past footer should succedd for "always past-tense"', t => {
+test('with past footer should succedd for "always past-tense"', t => {
 	const [actual] = footerTense(parsed.past, 'always', ['past-tense']);
 	const expected = true;
 	t.is(actual, expected);
 });
 
-test.failing('with past footer should fail for "never past-tense"', t => {
+test('with past footer should fail for "never past-tense"', t => {
 	const [actual] = footerTense(parsed.past, 'never', ['past-tense']);
 	const expected = false;
+	t.is(actual, expected);
+});
+
+test('with mixed footer should fail for "always present-third-person"', t => {
+	const [actual] = footerTense(parsed.mixed, 'always', ['present-third-person']);
+	const expected = false;
+	t.is(actual, expected);
+});
+
+test('with mixed footer should fail for "always present-imperative"', t => {
+	const [actual] = footerTense(parsed.mixed, 'always', ['present-imperative']);
+	const expected = false;
+	t.is(actual, expected);
+});
+
+test('with present footer should fail for "always present-participle"', t => {
+	const [actual] = footerTense(parsed.mixed, 'always', ['present-participle']);
+	const expected = false;
+	t.is(actual, expected);
+});
+
+test('with mixed footer should fail for "always past-tense"', t => {
+	const [actual] = footerTense(parsed.mixed, 'always', ['past-tense']);
+	const expected = false;
+	t.is(actual, expected);
+});
+
+test('with mixed footer should succeed for "always present-third-person, present-imperative, present-participle, past-tense"', t => {
+	const [actual] = footerTense(parsed.mixed, 'always', ['present-third-person', 'present-imperative', 'present-participle', 'past-tense']);
+	const expected = true;
+	t.is(actual, expected);
+});
+
+test('with mixed footer should succeed for "never allowed: present-third-person" and matching ignored: implements', t => {
+	const [actual] = footerTense(parsed.mixed, 'never', {
+		allowed: ['present-third-person'],
+		ignored: ['implements']
+	});
+	const expected = true;
 	t.is(actual, expected);
 });
