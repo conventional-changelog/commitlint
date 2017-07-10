79110404e663458009efd8ab78ac0abbf782d782
mario.nebl@sinnerschrader.com <mario.nebl@sinnerschrader.com>
2017-07-10 14:37:13 +0200

    feat(prompt-cli): add standalone prompt interface

---
+++ @commitlint/prompt-cli/cli.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+const execa = require('execa');
+const meow = require('meow');
+const prompter = require('@commitlint/prompt').prompter;
+
+const HELP = `
+	Usage
+		$ commit
+`;
+
+const _ = undefined;
+const prompt = () => prompter(_, commit);
+
+main(meow(HELP))
+	.catch(err => {
+		setTimeout(() => {
+			throw err;
+		});
+	});
+
+function main() {
+	return prompt();
+}
+
+function commit(message) {
+	const c = execa('git', ['commit', '-m', message]);
+	c.stdout.pipe(process.stdout);
+	c.stderr.pipe(process.stderr);
+}
---
+++ @commitlint/prompt-cli/readme.md
@@ -0,0 +1,15 @@
+> commit prompt using .commitlintrc
+
+# @commitlint/prompt-cli
+
+## Getting started
+
+```bash
+npm install --g @commitlint/prompt-cli @commitlint/config-angular
+echo '{"extends": ["@commitlint/config-angular"]}' > .commitlintrc
+```
+
+```bash
+git add .
+commit
+```
