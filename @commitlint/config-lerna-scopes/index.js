8f35daeea1a5f035ed23a842c66aeeb9976287b9
Quentin Roy <quentin@quentinroy.fr>
2017-10-09 17:20:53 +0800

    fix(config-lerna-scopes): fix package list get with recent lerna versions

--- @commitlint/config-lerna-scopes/index.js
+++ @commitlint/config-lerna-scopes/index.js
@@ -1,4 +1,5 @@
 const Repository = require('lerna/lib/Repository');
+const PackageUtilities = require('lerna/lib/PackageUtilities');
 
 module.exports = {
 	utils: {getPackages},
@@ -8,8 +9,12 @@ module.exports = {
 };
 
 function getPackages() {
-	const repo = new Repository(process.cwd());
-	return repo.packages
+	const cwd = process.cwd();
+	const repository = new Repository(cwd);
+	return PackageUtilities.getPackages({
+		packageConfigs: repository.packageConfigs,
+		rootPath: cwd
+	})
 		.map(pkg => pkg.name)
 		.map(name => (name.charAt(0) === '@' ? name.split('/')[1] : name));
 }
