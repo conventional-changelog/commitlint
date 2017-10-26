a04f12f1c74ae81b8d79b6f265a5f6e9a2853a6b
Mario Nebl <marionebl@users.noreply.github.com>
2017-10-26 21:20:53 +0200

    fix: default to process.cwd #100 (#101)

---
+++ @commitlint/core/src/load.serial-test.js
@@ -0,0 +1,19 @@
+import {fix} from '@commitlint/test';
+import test from 'ava';
+
+import load from './load';
+
+test.serial('default cwd option to process.cwd()', async t => {
+	const cwd = await fix.bootstrap('fixtures/basic');
+	const before = process.cwd();
+	process.chdir(cwd);
+
+	try {
+		const actual = await load();
+		t.true(actual.rules.basic);
+	} catch (err) {
+		throw err;
+	} finally {
+		process.chdir(before);
+	}
+});
