c4aa8405bd8ac9f32fc90e3726a717d8e8ce79eb
Mario Nebl <hello@mario-nebl.de>
2017-11-18 16:03:10 +0100

    feat(config-angular): disallow subject with uppercase first character
    BREAKING CHANGE
    
    Formerly working commit messages are now considered problems
    *  type: SOME MESSAGE
    *  type: SomeMessage
    *  type: Some Message

--- @commitlint/config-angular/index.js
+++ @commitlint/config-angular/index.js
@@ -5,10 +5,15 @@ module.exports = {
 		'body-leading-blank': [1, 'always'],
 		'footer-leading-blank': [1, 'always'],
 		'header-max-length': [2, 'always', 72],
-		'scope-case': [2, 'always', 'lowerCase'],
+		'scope-case': [2, 'always', 'lower-case'],
+		'subject-case': [
+			2,
+			'never',
+			['sentence-case', 'start-case', 'pascal-case', 'upper-case']
+		],
 		'subject-empty': [2, 'never'],
 		'subject-full-stop': [2, 'never', '.'],
-		'type-case': [2, 'always', 'lowerCase'],
+		'type-case': [2, 'always', 'lower-case'],
 		'type-empty': [2, 'never'],
 		'type-enum': typeEnum.rules['type-enum']
 	}
