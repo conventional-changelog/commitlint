c94c4dc44a9e8291af48be0108e68beaad6332b8
Mario Nebl <marionebl@users.noreply.github.com>
2017-12-26 19:48:42 +0100

    fix: ignore branch merges with newlines (#214)
    * fix: ignore branch merges with newlines
    
    * fix: remove stray console.log

--- @commitlint/is-ignored/src/index.test.js
+++ @commitlint/is-ignored/src/index.test.js
@@ -47,6 +47,11 @@ test('should return true for branch merges', t => {
 	t.true(isIgnored("Merge branch 'iss53'"));
 });
 
+test('should return true for branch merges with newline characters', t => {
+	t.true(isIgnored("Merge branch 'ctrom-YarnBuild'\n"));
+	t.true(isIgnored("Merge branch 'ctrom-YarnBuild'\r\n"));
+});
+
 test('should return true for merged PRs', t => {
 	t.true(isIgnored('Merge pull request #369'));
 });
