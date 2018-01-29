ecd1f6d2645e28a2a3313e70d39ea844e4762741
Mario Nebl <hello@mario-nebl.de>
2018-01-29 12:00:30 +0100

    refactor: derive strictly required files from package.json

--- @commitlint/cli/src/publish.test.js
+++ @commitlint/cli/src/publish.test.js
@@ -1,10 +1,27 @@
 import pkgDir from 'pkg-dir';
 import {npm} from '@commitlint/test';
 import test from 'ava';
+import expect from 'unexpected';
 
-test('should publish required files', async t => {
+test('should publish required files', async () => {
 	const cwd = await pkgDir(__dirname);
-	const actual = await npm.getTarballFiles({cwd});
-	const expected = await npm.getPackageFiles({cwd, npmignore: true});
-	t.deepEqual(actual, expected);
+
+	const actual = await npm.getTarballFiles(cwd);
+	const expected = await npm.getPackageFiles(cwd);
+
+	expect(actual, 'to contain', ...expected.bin);
+	expect(actual, 'to contain', expected.main);
+
+	expect(actual, 'to have items satisfying', item => {
+		expect(item, 'not to end with', '.test.js');
+	});
+});
+
+test('should not publish test files', async () => {
+	const cwd = await pkgDir(__dirname);
+	const actual = await npm.getTarballFiles(cwd);
+
+	expect(actual, 'to have items satisfying', item => {
+		expect(item, 'not to end with', '.test.js');
+	});
 });
--- @packages/test/src/npm.js
+++ @packages/test/src/npm.js
@@ -1,11 +1,11 @@
 import path from 'path';
 import fs from 'fs';
 import zlib from 'zlib';
-import * as sander from '@marionebl/sander';
 import execa from 'execa';
-import globby from 'globby';
+import readPkg from 'read-pkg';
+import * as sander from '@marionebl/sander';
 import tar from 'tar-fs';
-import packlist from 'npm-packlist';
+import values from 'lodash.values';
 
 import * as fix from './fix';
 import * as git from './git';
@@ -22,37 +22,52 @@ async function bootstrap(fixture) {
 	return cwd;
 }
 
-async function getTarballFiles({cwd}) {
-	const tarballFilename = await execa.stdout('npm', ['pack'], {cwd});
-	const tarballFile = path.resolve(cwd, tarballFilename);
-	const workDir = await fix.bootstrap();
-
-	try {
-		// Extract tarball
-		await new Promise((resolve, reject) => {
-			fs
-				.createReadStream(tarballFile)
-				.pipe(zlib.createGunzip())
-				.pipe(tar.extract(workDir))
-				.once('error', err => reject(err))
-				.once('finish', () => resolve());
-		});
-
-		// List all files
-		return (await globby(['**'], {
-			cwd: path.resolve(workDir, 'package'),
-			dot: true,
-			nodir: true,
-			nosort: true
-		})).sort();
-	} finally {
-		sander.rimraf(tarballFile);
-		sander.rimraf(workDir);
-	}
+async function getTarballFiles(source) {
+	const cwd = await fix.bootstrap(source);
+	const tarball = await execa.stdout('npm', ['pack'], {cwd});
+	const files = await getArchveFiles(path.join(cwd, tarball));
+	return files;
 }
 
-async function getPackageFiles({cwd, npmignore}) {
-	return (await packlist({path: cwd}))
-		.concat(npmignore ? '.npmignore' : [])
-		.sort();
+function getArchveFiles(filePath) {
+	return new Promise((resolve, reject) => {
+		const files = [];
+
+		fs
+			.createReadStream(filePath)
+			.pipe(zlib.createGunzip())
+			.pipe(
+				tar.extract(path.dirname(filePath), {
+					ignore(_, header) {
+						files.push(path.relative('package', header.name));
+						return true;
+					}
+				})
+			)
+			.once('error', err => reject(err))
+			.once('finish', () => resolve(files));
+	});
+}
+
+async function getPackageFiles(source) {
+	const pkg = await readPkg(source);
+
+	return {
+		main: path.normalize(pkg.main || './index.js'),
+		bin: getPkgBinFiles(pkg.bin)
+	};
+}
+
+function getPkgBinFiles(bin) {
+	if (!bin) {
+		return [];
+	}
+
+	if (typeof bin === 'string') {
+		return [path.normalize(bin)];
+	}
+
+	if (typeof bin === 'object') {
+		return values(bin).map(b => path.normalize(b));
+	}
 }
