d7bbc285d9ccfe0d50e14a4e67928e73a8bc4a86
evilebottnawi <alexander.krasnoyarov@itgalaxy.company>
2018-05-02 18:13:22 +0300

    fix: support any case in single, double and back quotes

--- @commitlint/ensure/src/case.test.js
+++ @commitlint/ensure/src/case.test.js
@@ -106,11 +106,188 @@ test('true for * on snake-case', t => {
 });
 
 test('true for * on pascal-case', t => {
-	const actual = ensure('*', 'snake-case');
+	const actual = ensure('*', 'pascal-case');
 	t.is(actual, true);
 });
 
 test('true for * on start-case', t => {
-	const actual = ensure('*', 'snake-case');
+	const actual = ensure('*', 'start-case');
 	t.is(actual, true);
+});
+
+test('true for `Any_CASE_iN_back-quotes` on lowercase', t => {
+	const actual = ensure('`Any_CASE_iN_back-quotes`', 'lowercase');
+	t.is(actual, true);
+});
+
+test('true for `Any_CASE_iN_back-quotes` on uppercase', t => {
+	const actual = ensure('`Any_CASE_iN_back-quotes`', 'uppercase');
+	t.is(actual, true);
+});
+
+test('true for `Any_CASE_iN_back-quotes` on sentence-case', t => {
+	const actual = ensure('`Any_CASE_iN_back-quotes`', 'sentence-case');
+	t.is(actual, true);
+});
+
+test('true for `Any_CASE_iN_back-quotes` on camel-case', t => {
+	const actual = ensure('`Any_CASE_iN_back-quotes`', 'camel-case');
+	t.is(actual, true);
+});
+
+test('true for `Any_CASE_iN_back-quotes` on kebab-case', t => {
+	const actual = ensure('`Any_CASE_iN_back-quotes`', 'kebab-case');
+	t.is(actual, true);
+});
+
+test('true for `Any_CASE_iN_back-quotes` on snake-case', t => {
+	const actual = ensure('`Any_CASE_iN_back-quotes`', 'snake-case');
+	t.is(actual, true);
+});
+
+test('true for `Any_CASE_iN_back-quotes` on pascal-case', t => {
+	const actual = ensure('`Any_CASE_iN_back-quotes`', 'pascal-case');
+	t.is(actual, true);
+});
+
+test('true for `Any_CASE_iN_back-quotes` on start-case', t => {
+	const actual = ensure('`Any_CASE_iN_back-quotes`', 'start-case');
+	t.is(actual, true);
+});
+
+test('true for lowercase `Any_CASE_iN_back-quotes` lowercase on lowercase', t => {
+	const actual = ensure(
+		'lowercase `Any_CASE_iN_back-quotes` lowercase',
+		'lowercase'
+	);
+	t.is(actual, true);
+});
+
+test('false for UPPERCASE `Any_CASE_iN_back-quotes` UPPERCASE on lowercase', t => {
+	const actual = ensure(
+		'UPPERCASE `Any_CASE_iN_back-quotes` UPPERCASE',
+		'lowercase'
+	);
+	t.is(actual, false);
+});
+
+test('true for UPPERCASE `Any_CASE_iN_back-quotes` UPPERCASE on uppercase', t => {
+	const actual = ensure(
+		'UPPERCASE `Any_CASE_iN_back-quotes` UPPERCASE',
+		'uppercase'
+	);
+	t.is(actual, true);
+});
+
+test('false for lowercase `Any_CASE_iN_back-quotes` lowercase on uppercase', t => {
+	const actual = ensure(
+		'lowercase `Any_CASE_iN_back-quotes` lowercase',
+		'uppercase'
+	);
+	t.is(actual, false);
+});
+
+test('true for fooBar`Any_CASE_iN_back-quotes`fooBar on camel-case', t => {
+	const actual = ensure('fooBar`Any_CASE_iN_back-quotes`fooBar', 'camel-case');
+	t.is(actual, true);
+});
+
+test('false for Foo Bar`Any_CASE_iN_back-quotes` Foo Bar on camel-case', t => {
+	const actual = ensure(
+		'Foo Bar`Any_CASE_iN_back-quotes` Foo Bar',
+		'camel-case'
+	);
+	t.is(actual, false);
+});
+
+test('true for foo-bar`Any_CASE_iN_back-quotes`foo-bar on kebab-case', t => {
+	const actual = ensure(
+		'foo-bar`Any_CASE_iN_back-quotes`foo-bar',
+		'kebab-case'
+	);
+	t.is(actual, true);
+});
+
+test('false for Foo Bar `Any_CASE_iN_back-quotes` Foo Bar on kebab-case', t => {
+	const actual = ensure(
+		'Foo Bar `Any_CASE_iN_back-quotes` Foo Bar',
+		'kebab-case'
+	);
+	t.is(actual, false);
+});
+
+test('true for foo_bar`Any_CASE_iN_back-quotes`foo_bar on snake-case', t => {
+	const actual = ensure(
+		'foo_bar`Any_CASE_iN_back-quotes`foo_bar',
+		'snake-case'
+	);
+	t.is(actual, true);
+});
+
+test('false for Foo Bar `Any_CASE_iN_back-quotes` Foo Bar on snake-case', t => {
+	const actual = ensure(
+		'Foo Bar `Any_CASE_iN_back-quotes` Foo Bar',
+		'snake-case'
+	);
+	t.is(actual, false);
+});
+
+test('true for PascalCase`Any_CASE_iN_back-quotes`PascalCase on pascal-case', t => {
+	const actual = ensure(
+		'PascalCase`Any_CASE_iN_back-quotes`PascalCase',
+		'pascal-case'
+	);
+	t.is(actual, true);
+});
+
+test('false for Foo Bar `Any_CASE_iN_back-quotes` Foo Bar on pascal-case', t => {
+	const actual = ensure(
+		'Foo Bar `Any_CASE_iN_back-quotes` Foo Bar',
+		'pascal-case'
+	);
+	t.is(actual, false);
+});
+
+test('true for Foo Bar`Any_CASE_iN_back-quotes` Foo Bar on start-case', t => {
+	const actual = ensure(
+		'Foo Bar `Any_CASE_iN_back-quotes`Foo Bar',
+		'start-case'
+	);
+	t.is(actual, true);
+});
+
+test('false for foo_bar`Any_CASE_iN_back-quotes`foo_bar on start-case', t => {
+	const actual = ensure(
+		'foo_bar`Any_CASE_iN_back-quotes`foo_bar',
+		'start-case'
+	);
+	t.is(actual, false);
+});
+
+test('true for lowercase `Any_CASE_iN_back-quotes` `Any_CASE_iN_back-quotes` lowercase on lowercase', t => {
+	const actual = ensure(
+		'lowercase `Any_CASE_iN_back-quotes` `Any_CASE_iN_back-quotes` lowercase',
+		'lowercase'
+	);
+	t.is(actual, true);
+});
+
+test("true for 'Any_CASE_iN_single-quotes' on lowercase", t => {
+	const actual = ensure("'Any_CASE_iN_single-quotes'", 'lowercase');
+	t.is(actual, true);
+});
+
+test('true for "Any_CASE_iN_double-quotes" on lowercase', t => {
+	const actual = ensure('"Any_CASE_iN_double-quotes"', 'lowercase');
+	t.is(actual, true);
+});
+
+test('true for `lowercasel"\'` on lowercase', t => {
+	const actual = ensure('`lowercasel"\'`', 'lowercase');
+	t.is(actual, true);
+});
+
+test('false for `LOWERCASE on lowercase', t => {
+	const actual = ensure('`LOWERCASE', 'lowercase');
+	t.is(actual, false);
 });
