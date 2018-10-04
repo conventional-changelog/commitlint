1b16ad156813abbf81157e44312b94ea0e955f90
dabit1gamma <39829116+dabit1gamma@users.noreply.github.com>
2018-10-04 18:29:33 +0200

    Update index.test.js

--- @commitlint/is-ignored/src/index.test.js
+++ @commitlint/is-ignored/src/index.test.js
@@ -57,6 +57,11 @@ test('should return true for branch merges with multiple newline characters', t 
 	t.true(isIgnored("Merge branch 'ctrom-YarnBuild'\r\n\r\n\r\n"));
 });
 
+test('should return true for branch merges with newline characters and more characters after it', t => {
+	t.true(isIgnored("Merge branch 'ctrom-YarnBuild'\n "));
+	t.true(isIgnored("Merge branch 'ctrom-YarnBuild'\r\n # some comment"));
+});
+
 test('should return true for merged PRs', t => {
 	t.true(isIgnored('Merge pull request #369'));
 });
