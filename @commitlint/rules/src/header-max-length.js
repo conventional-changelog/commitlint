c0863f90a312738826ec6b1e97af56275114f51b
hustcc <i@hust.cc>
2019-01-31 09:46:49 +0800

    feat(rule-header-length): show current header length

--- @commitlint/rules/src/header-max-length.js
+++ @commitlint/rules/src/header-max-length.js
@@ -3,6 +3,8 @@ import {maxLength} from '@commitlint/ensure';
 export default (parsed, when, value) => {
 	return [
 		maxLength(parsed.header, value),
-		`header must not be longer than ${value} characters`
+		`header must not be longer than ${value} characters, current length is ${
+			parsed.header.length
+		}`
 	];
 };
