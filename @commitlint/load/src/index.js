36d55270a8a95e43bccb1aa32a9e4a011c4ee742
Mario Nebl <hello@mario-nebl.de>
2019-03-28 13:59:15 +0100

    fix: adapt to new cosmiconfig api

--- @commitlint/load/src/index.js
+++ @commitlint/load/src/index.js
@@ -86,11 +86,12 @@ export default async (seed = {}, options = {cwd: process.cwd()}) => {
 };
 
 async function loadConfig(cwd, configPath) {
-	const explorer = cosmiconfig('commitlint', {
-		searchPlaces: configPath ? [path.resolve(cwd, configPath)] : []
-	});
+	const explorer = cosmiconfig('commitlint');
 
-	const local = await explorer.load(cwd);
+	const explicitPath = configPath ? path.join(cwd, configPath) : undefined;
+	const explore = explicitPath ? explorer.load : explorer.search;
+	const searchPath = explicitPath ? explicitPath : cwd;
+	const local = await explore(searchPath);
 
 	if (local) {
 		return local;
