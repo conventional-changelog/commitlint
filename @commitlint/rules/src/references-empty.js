be534703ee3b4feca7231198565e3b0693e05a35
WangYunjian <wangyunjian@kingsoft.com>
2019-04-04 20:32:12 +0800

    feat(rules): add value to references-empty
    lint references-empty when type-enum is within the specified type

--- @commitlint/rules/src/references-empty.js
+++ @commitlint/rules/src/references-empty.js
@@ -1,10 +1,19 @@
 import message from '@commitlint/message';
 
-export default (parsed, when = 'never') => {
+export default (parsed, when = 'never', value = []) => {
+	const notEmptyType = value.indexOf(parsed.type) !== -1;
+	if (value.length > 0 && !notEmptyType) {
+		return [true];
+	}
 	const negated = when === 'always';
 	const notEmpty = parsed.references.length > 0;
 	return [
 		negated ? !notEmpty : notEmpty,
-		message(['references', negated ? 'must' : 'may not', 'be empty'])
+		message([
+			'references',
+			negated ? 'must' : 'may not',
+			'be empty',
+			notEmptyType ? `when type of [${value.join(', ')}]` : ''
+		])
 	];
 };
