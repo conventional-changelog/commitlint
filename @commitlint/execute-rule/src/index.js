1ed6503e8c27d594a83dc9bff1c2d7f21422f8ea
Mario Nebl <hello@mario-nebl.de>
2019-05-20 17:51:43 +0000

    build: port execute-rule to ts

--- @commitlint/execute-rule/src/index.js
+++
@@ -1,9 +0,0 @@
-export default async entry => {
-	if (!Array.isArray(entry)) {
-		return null;
-	}
-	const [name, config] = entry;
-	return typeof config === 'function'
-		? [name, await config()]
-		: [name, await config];
-};
