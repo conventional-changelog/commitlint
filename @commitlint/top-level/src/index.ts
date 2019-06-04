5f4c746d1465065658390ff3a0f28280a54adeb5
Cedric van Putten <me@bycedric.com>
2019-06-04 23:32:44 +0200

    refactor(top-level): rewrite top level to typescript

---
+++ @commitlint/top-level/src/index.ts
@@ -0,0 +1,17 @@
+import path from 'path';
+import up from 'find-up';
+
+export default toplevel;
+
+/**
+ * Find the next git root
+ */
+async function toplevel(cwd: string) {
+	const found = await up('.git', { cwd, type: 'directory' });
+
+	if (typeof found !== 'string') {
+		return found;
+	}
+
+	return path.join(found, '..');
+}
