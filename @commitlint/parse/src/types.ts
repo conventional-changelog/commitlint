756a333fda314e8f32dc858b5bf202a02b2cf731
Cedric van Putten <me@bycedric.com>
2019-09-11 16:46:49 +0200

    refactor: port parse to typescript (#764)
    * refactor: port parse to typescript
    
    * refactor: add commit interface to parse return type
    
    * refactor: remove replaced dependencies from parse
    
    * refactor(parse): add strict options interface
    
    * refactor(parse): add strict parser type
    
    * test(parse): add empty and no commit message tests

---
+++ @commitlint/parse/src/types.ts
@@ -0,0 +1,45 @@
+export interface Commit {
+	raw: string;
+	header: string;
+	type: string | null;
+	scope: string | null;
+	subject: string | null;
+	body: string | null;
+	footer: string | null;
+	mentions: string[];
+	notes: CommitNote[];
+	references: CommitReference[];
+	revert: any;
+	merge: any;
+}
+
+export interface CommitNote {
+	title: string;
+	text: string;
+}
+
+export interface CommitReference {
+	raw: string;
+	prefix: string;
+	action: string | null;
+	owner: string | null;
+	repository: string | null;
+	issue: string | null;
+}
+
+export type Parser = (
+	message: string,
+	options: ParserOptions
+) => Omit<Commit, 'raw'>;
+
+export interface ParserOptions {
+	commentChar?: string;
+	headerCorrespondence?: string[];
+	headerPattern?: RegExp;
+	issuePrefixes?: string[];
+	mergeCorrespondence?: string[];
+	mergePattern?: RegExp;
+	noteKeywords?: string[];
+	revertCorrespondence?: string[];
+	revertPattern?: RegExp;
+}
