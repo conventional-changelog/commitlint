bbe35d87c8770237ac40226535c4264155f1da69
Kristóf Poduszló <kripod@protonmail.com>
2019-10-16 18:57:07 +0200

    Allow any scope casing for Conventional Commits
    As the [spec](https://www.conventionalcommits.org/en/v1.0.0/) does not enforce any scope casing rules, I think that [any casing may be used](https://www.conventionalcommits.org/en/v1.0.0/#are-the-types-in-the-commit-title-uppercase-or-lowercase) on behalf of repo maintainers.

--- @commitlint/config-conventional/index.js
+++ @commitlint/config-conventional/index.js
@@ -3,7 +3,6 @@ module.exports = {
 		'body-leading-blank': [1, 'always'],
 		'footer-leading-blank': [1, 'always'],
 		'header-max-length': [2, 'always', 72],
-		'scope-case': [2, 'always', 'lower-case'],
 		'subject-case': [
 			2,
 			'never',
