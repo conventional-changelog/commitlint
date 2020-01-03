741aade5a125bcd0a3c8a356f5b07dcd5cf14f77
Armano <armano2@users.noreply.github.com>
2020-01-03 23:16:06 +0100

    chore: update rule names (import)

--- @commitlint/rules/src/index.js
+++ @commitlint/rules/src/index.js
@@ -1,67 +1,67 @@
-import bodycase from './body-case';
-import bodyempty from './body-empty';
-import bodyleadingblank from './body-leading-blank';
-import bodymaxlength from './body-max-length';
-import bodymaxlinelength from './body-max-line-length';
-import bodyminlength from './body-min-length';
-import footerempty from './footer-empty';
-import footerleadingblank from './footer-leading-blank';
-import footermaxlength from './footer-max-length';
-import footermaxlinelength from './footer-max-line-length';
-import footerminlength from './footer-min-length';
-import headercase from './header-case';
-import headerfullstop from './header-full-stop';
-import headermaxlength from './header-max-length';
-import headerminlength from './header-min-length';
-import referencesempty from './references-empty';
-import scopecase from './scope-case';
-import scopeempty from './scope-empty';
-import scopeenum from './scope-enum';
-import scopemaxlength from './scope-max-length';
-import scopeminlength from './scope-min-length';
-import signedoffby from './signed-off-by';
-import subjectcase from './subject-case';
-import subjectempty from './subject-empty';
-import subjectfullstop from './subject-full-stop';
-import subjectmaxlength from './subject-max-length';
-import subjectminlength from './subject-min-length';
-import typecase from './type-case';
-import typeempty from './type-empty';
-import typeenum from './type-enum';
-import typemaxlength from './type-max-length';
-import typeminlength from './type-min-length';
+import bodyCase from './body-case';
+import bodyEmpty from './body-empty';
+import bodyLeadingBlank from './body-leading-blank';
+import bodyMaxLength from './body-max-length';
+import bodyMaxLineLength from './body-max-line-length';
+import bodyMinLength from './body-min-length';
+import footerEmpty from './footer-empty';
+import footerLeadingBlank from './footer-leading-blank';
+import footerMaxLength from './footer-max-length';
+import footerMaxLineLength from './footer-max-line-length';
+import footerMinLength from './footer-min-length';
+import headerCase from './header-case';
+import headerFullStop from './header-full-stop';
+import headerMaxLength from './header-max-length';
+import headerMinLength from './header-min-length';
+import referencesEmpty from './references-empty';
+import scopeCase from './scope-case';
+import scopeEmpty from './scope-empty';
+import scopeEnum from './scope-enum';
+import scopeMaxLength from './scope-max-length';
+import scopeMinLength from './scope-min-length';
+import signedOffBy from './signed-off-by';
+import subjectCase from './subject-case';
+import subjectEmpty from './subject-empty';
+import subjectFullStop from './subject-full-stop';
+import subjectMaxLength from './subject-max-length';
+import subjectMinLength from './subject-min-length';
+import typeCase from './type-case';
+import typeEmpty from './type-empty';
+import typeEnum from './type-enum';
+import typeMaxLength from './type-max-length';
+import typeMinLength from './type-min-length';
 
 export default {
-	'body-case': bodycase,
-	'body-empty': bodyempty,
-	'body-leading-blank': bodyleadingblank,
-	'body-max-length': bodymaxlength,
-	'body-max-line-length': bodymaxlinelength,
-	'body-min-length': bodyminlength,
-	'footer-empty': footerempty,
-	'footer-leading-blank': footerleadingblank,
-	'footer-max-length': footermaxlength,
-	'footer-max-line-length': footermaxlinelength,
-	'footer-min-length': footerminlength,
-	'header-case': headercase,
-	'header-full-stop': headerfullstop,
-	'header-max-length': headermaxlength,
-	'header-min-length': headerminlength,
-	'references-empty': referencesempty,
-	'scope-case': scopecase,
-	'scope-empty': scopeempty,
-	'scope-enum': scopeenum,
-	'scope-max-length': scopemaxlength,
-	'scope-min-length': scopeminlength,
-	'signed-off-by': signedoffby,
-	'subject-case': subjectcase,
-	'subject-empty': subjectempty,
-	'subject-full-stop': subjectfullstop,
-	'subject-max-length': subjectmaxlength,
-	'subject-min-length': subjectminlength,
-	'type-case': typecase,
-	'type-empty': typeempty,
-	'type-enum': typeenum,
-	'type-max-length': typemaxlength,
-	'type-min-length': typeminlength
+	'body-case': bodyCase,
+	'body-empty': bodyEmpty,
+	'body-leading-blank': bodyLeadingBlank,
+	'body-max-length': bodyMaxLength,
+	'body-max-line-length': bodyMaxLineLength,
+	'body-min-length': bodyMinLength,
+	'footer-empty': footerEmpty,
+	'footer-leading-blank': footerLeadingBlank,
+	'footer-max-length': footerMaxLength,
+	'footer-max-line-length': footerMaxLineLength,
+	'footer-min-length': footerMinLength,
+	'header-case': headerCase,
+	'header-full-stop': headerFullStop,
+	'header-max-length': headerMaxLength,
+	'header-min-length': headerMinLength,
+	'references-empty': referencesEmpty,
+	'scope-case': scopeCase,
+	'scope-empty': scopeEmpty,
+	'scope-enum': scopeEnum,
+	'scope-max-length': scopeMaxLength,
+	'scope-min-length': scopeMinLength,
+	'signed-off-by': signedOffBy,
+	'subject-case': subjectCase,
+	'subject-empty': subjectEmpty,
+	'subject-full-stop': subjectFullStop,
+	'subject-max-length': subjectMaxLength,
+	'subject-min-length': subjectMinLength,
+	'type-case': typeCase,
+	'type-empty': typeEmpty,
+	'type-enum': typeEnum,
+	'type-max-length': typeMaxLength,
+	'type-min-length': typeMinLength
 };
