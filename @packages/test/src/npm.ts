d678b9aa41e8c44c2093f50b3c9e034418501fd8
Armano <armano2@users.noreply.github.com>
2020-01-26 18:57:28 +0100

    chore: refactor test utils to typescript

--- @packages/test/src/npm.ts
+++ @packages/test/src/npm.ts
@@ -1,14 +1,13 @@
 import execa from 'execa';
-import * as sander from '@marionebl/sander';
+import path from 'path';
+import fs from 'fs-extra';
 
 import * as git from './git';
 
-export {bootstrap};
-
-async function bootstrap(fixture) {
+export async function bootstrap(fixture: string) {
 	const cwd = await git.bootstrap(fixture);
 
-	if (await sander.exists(cwd, 'package.json')) {
+	if (await fs.pathExists(path.join(cwd, 'package.json'))) {
 		await execa('npm', ['install'], {cwd});
 	}
 
