150b5de3c93260dd5d8d58da156e3e9b9c5939e5
Cedric van Putten <me@bycedric.com>
2020-01-26 14:54:49 +0000

    chore: remove javascript rules pattern from jest

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@ module.exports = {
 	preset: 'ts-jest/presets/js-with-babel',
 	testEnvironment: 'node',
 	testRegex: undefined,
-	testMatch: ['**/*.test.ts?(x)', '**/@commitlint/rules/src/*.test.js?(x)']
+	testMatch: ['**/*.test.ts?(x)']
 };
