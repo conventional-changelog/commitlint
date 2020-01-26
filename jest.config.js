cddbf09cb31a0e264b781d994a32c9c161953302
Armano <armano2@users.noreply.github.com>
2020-01-26 14:52:56 +0100

    test(rules): migrate read tests from ava to jest

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,9 @@ module.exports = {
 	preset: 'ts-jest/presets/js-with-babel',
 	testEnvironment: 'node',
 	testRegex: undefined,
-	testMatch: ['**/*.test.ts?(x)', '**/@commitlint/rules/src/*.test.js?(x)']
+	testMatch: [
+		'**/*.test.ts?(x)',
+		'**/@commitlint/rules/src/*.test.js?(x)',
+		'**/@commitlint/read/src/*.test.js?(x)'
+	]
 };
