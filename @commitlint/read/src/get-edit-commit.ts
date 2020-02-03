f0dd45d315c98ea32999b914ca1792c6368055f2
Mario Nebl <marionebl@users.noreply.github.com>
2020-02-03 20:03:14 +1100

    refactor: port read to ts (#922)
    * refactor: port read to typescript
    
    * fix: use @types/git-raw-commits
    
    * fix: adapt to latest master
    
    * refactor: unnest getEditFilePath
    
    * style: apply autoformatting

---
+++ @commitlint/read/src/get-edit-commit.ts
@@ -0,0 +1,21 @@
+import toplevel from '@commitlint/top-level';
+import {getEditFilePath} from './get-edit-file-path';
+
+const sander = require('@marionebl/sander');
+
+// Get recently edited commit message
+export async function getEditCommit(
+	cwd?: string,
+	edit?: boolean | string
+): Promise<string[]> {
+	const top = await toplevel(cwd);
+
+	if (typeof top !== 'string') {
+		throw new TypeError(`Could not find git root from ${cwd}`);
+	}
+
+	const editFilePath = await getEditFilePath(top, edit);
+
+	const editFile: Buffer = await sander.readFile(editFilePath);
+	return [`${editFile.toString('utf-8')}\n`];
+}
