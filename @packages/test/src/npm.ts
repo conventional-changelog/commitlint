186c002302e8237486cb4c5c4e73f1fa0b843f7f
Mario Nebl <mnebl@atlassian.com>
2020-02-06 09:20:38 +1100

    test: speed up npm bootstrap

--- @packages/test/src/npm.ts
+++ @packages/test/src/npm.ts
@@ -1,15 +1,62 @@
-import execa from 'execa';
 import path from 'path';
 import fs from 'fs-extra';
+import resolvePkg from 'resolve-pkg';
 
 import * as git from './git';
 
 export async function bootstrap(fixture: string, directory?: string) {
 	const cwd = await git.bootstrap(fixture, directory);
+	const manifestPath = path.join(cwd, 'package.json');
+	const targetModulesPath = path.join(cwd, 'node_modules');
 
-	if (await fs.pathExists(path.join(cwd, 'package.json'))) {
-		await execa('npm', ['install'], {cwd});
+	if (await fs.pathExists(manifestPath)) {
+		const {dependencies = {}, devDependencies = {}} = await fs.readJson(
+			manifestPath
+		);
+		const deps = Object.keys({...dependencies, ...devDependencies});
+		await Promise.all(
+			deps.map(async (dependency: any) => {
+				const sourcePath = resolvePkg(dependency);
+
+				if (!sourcePath) {
+					throw new Error(`Could not resolve dependency ${dependency}`);
+				}
+
+				const sourceModulesPath = findParentPath(sourcePath, 'node_modules');
+
+				if (!sourceModulesPath) {
+					throw new Error(`Could not determine node_modules for ${sourcePath}`);
+				}
+
+				const relativePath = path.relative(sourceModulesPath, sourcePath);
+				const targetPath = path.join(targetModulesPath, relativePath);
+
+				await fs.mkdirp(path.join(targetPath, '..'));
+				await fs.symlink(sourcePath, targetPath);
+			})
+		);
 	}
 
 	return cwd;
+}
+
+function findParentPath(path: string, dirname: string): string | undefined {
+	const rawFragments = path.split('/');
+
+	const {matched, fragments} = rawFragments.reduceRight(
+		({fragments, matched}, item) => {
+			if (item === dirname && !matched) {
+				return {fragments, matched: true};
+			}
+
+			if (!matched && fragments.length > 0) {
+				fragments.pop();
+			}
+
+			return {fragments, matched};
+		},
+		{fragments: rawFragments, matched: false}
+	);
+
+	return matched ? fragments.join('/') : undefined;
 }
