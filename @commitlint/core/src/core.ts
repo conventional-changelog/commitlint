b5ba369aa2765c23bddcc035b5c07d43de27010c
Armano <armano2@users.noreply.github.com>
2020-02-06 04:29:39 +0100

    refactor: port core to typescript (#961)
    * refactor(core): port core to typescript
    
    * refactor(core): rename file and fix formatting

---
+++ @commitlint/core/src/core.ts
@@ -0,0 +1,11 @@
+import format from '@commitlint/format';
+import load from '@commitlint/load';
+import lint from '@commitlint/lint';
+import read from '@commitlint/read';
+
+export = {
+	format,
+	load,
+	lint,
+	read
+};
