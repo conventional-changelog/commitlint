dc46938b13289c1a965b2fbaf9c64dd4d30bb3e3
Armano <armano2@users.noreply.github.com>
2020-02-06 22:11:54 +0100

    refactor(commitlint): simplify commitlint alias package

---
+++ @alias/commitlint/cli.test.js
@@ -0,0 +1,28 @@
+import execa from 'execa';
+import {fix} from '@commitlint/test';
+
+const bin = require.resolve('./cli.js');
+
+function cli(args, options, input) {
+	const c = execa(bin, args, {
+		cwd: options.cwd,
+		env: options.env,
+		input: input
+	});
+	return c.catch(err => err);
+}
+
+const fixBootstrap = fixture => fix.bootstrap(fixture, __dirname);
+
+test('should reprint input from stdin', async () => {
+	const cwd = await fixBootstrap('fixtures/default');
+	const actual = await cli([], {cwd}, 'foo: bar');
+	expect(actual.stdout).toContain('foo: bar');
+});
+
+test('should produce success output with --verbose flag', async () => {
+	const cwd = await fixBootstrap('fixtures/default');
+	const actual = await cli(['--verbose'], {cwd}, 'type: bar');
+	expect(actual.stdout).toContain('0 problems, 0 warnings');
+	expect(actual.stderr).toEqual('');
+});
