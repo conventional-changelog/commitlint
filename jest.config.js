a2deebc4612b5d48eef707fd9d2054850532449b
Armano <armano2@users.noreply.github.com>
2020-02-07 01:24:41 +0100

    test: disable babel build for tests and simplify jest config

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,5 @@ module.exports = {
 	preset: 'ts-jest/presets/js-with-babel',
 	testEnvironment: '@commitlint/test-environment',
 	testRegex: undefined,
-	testMatch: [
-		'**/*.test.ts?(x)',
-		'**/@commitlint/{lint,read,travis-cli,cli,load,prompt}/src/**/*.test.js?(x)',
-		'**/@commitlint/{prompt-cli,config-lerna-scopes}/*.test.js?(x)',
-		'**/@alias/commitlint/*.test.js?(x)'
-	]
+	testMatch: ['**/*.test.[jt]s?(x)']
 };
