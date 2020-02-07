898d5fa71e4bfed41a30579b7688d1e5e04bb199
Armano <armano2@users.noreply.github.com>
2020-02-08 00:24:50 +0100

    refactor(parse): refactor parse to use object spread

--- @commitlint/parse/src/index.ts
+++ @commitlint/parse/src/index.ts
@@ -1,20 +1,18 @@
-import mergeWith from 'lodash/mergeWith';
 import {Commit, Parser, ParserOptions} from '@commitlint/types';
 
 const {sync} = require('conventional-commits-parser');
 const defaultChangelogOpts = require('conventional-changelog-angular');
 
-export default parse;
-
-async function parse(
+export default async function parse(
 	message: string,
 	parser: Parser = sync,
 	parserOpts?: ParserOptions
 ): Promise<Commit> {
 	const defaultOpts = (await defaultChangelogOpts).parserOpts;
-	const opts = mergeWith({}, defaultOpts, parserOpts, (objValue, srcValue) => {
-		if (Array.isArray(objValue)) return srcValue;
-	});
+	const opts = {
+		...defaultOpts,
+		...(parserOpts || {})
+	};
 	const parsed = parser(message, opts) as Commit;
 	parsed.raw = message;
 	return parsed;
