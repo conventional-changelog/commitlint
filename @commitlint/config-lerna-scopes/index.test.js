1b53445892e4904742d32c787aa86468ff2189cb
Armano <armano2@users.noreply.github.com>
2020-02-08 17:46:46 +0100

    test(config-lerna-scopes): add regression tests

--- @commitlint/config-lerna-scopes/index.test.js
+++ @commitlint/config-lerna-scopes/index.test.js
@@ -1,5 +1,6 @@
 import {lerna} from '@commitlint/test';
 import config from '.';
+import semver from 'semver';
 
 test('exports rules key', () => {
 	expect(config).toHaveProperty('rules');
@@ -28,47 +29,61 @@ test('scope-enum is function', () => {
 });
 
 test('scope-enum does not throw for missing context', async () => {
+	const semverLt = jest.spyOn(semver, 'lt');
 	const {'scope-enum': fn} = config.rules;
 	await expect(fn()).resolves.toBeTruthy();
+	expect(semverLt).toHaveBeenLastCalledWith('3.20.2', '3.0.0');
 });
 
 test('scope-enum has expected severity', async () => {
+	const semverLt = jest.spyOn(semver, 'lt');
 	const {'scope-enum': fn} = config.rules;
 	const [severity] = await fn();
 	expect(severity).toBe(2);
+	expect(semverLt).toHaveBeenLastCalledWith('3.20.2', '3.0.0');
 });
 
 test('scope-enum has expected modifier', async () => {
+	const semverLt = jest.spyOn(semver, 'lt');
 	const {'scope-enum': fn} = config.rules;
 	const [, modifier] = await fn();
 	expect(modifier).toBe('always');
+	expect(semverLt).toHaveBeenLastCalledWith('3.20.2', '3.0.0');
 });
 
 test('returns empty value for empty lerna repository', async () => {
+	const semverLt = jest.spyOn(semver, 'lt');
 	const {'scope-enum': fn} = config.rules;
 	const cwd = await lerna.bootstrap('empty', __dirname);
 	const [, , value] = await fn({cwd});
 	expect(value).toEqual([]);
+	expect(semverLt).toHaveBeenLastCalledWith('3.20.2', '3.0.0');
 });
 
 test('returns expected value for basic lerna repository', async () => {
+	const semverLt = jest.spyOn(semver, 'lt');
 	const {'scope-enum': fn} = config.rules;
 	const cwd = await lerna.bootstrap('basic', __dirname);
 
 	const [, , value] = await fn({cwd});
 	expect(value).toEqual(['a', 'b']);
+	expect(semverLt).toHaveBeenLastCalledWith('3.20.2', '3.0.0');
 });
 
 test('returns expected value for scoped lerna repository', async () => {
+	const semverLt = jest.spyOn(semver, 'lt');
 	const {'scope-enum': fn} = config.rules;
 	const cwd = await lerna.bootstrap('scoped', __dirname);
 
 	const [, , value] = await fn({cwd});
 	expect(value).toEqual(['a', 'b']);
+	expect(semverLt).toHaveBeenLastCalledWith('3.20.2', '3.0.0');
 });
 
 test('works with lerna version < 3', async () => {
+	const semverLt = jest.spyOn(semver, 'lt');
 	const {'scope-enum': fn} = config.rules;
 	const cwd = await lerna.bootstrap('lerna-two', __dirname);
 	await expect(fn({cwd})).resolves.toBeTruthy();
+	expect(semverLt).toHaveBeenLastCalledWith('2.11.0', '3.0.0');
 });
