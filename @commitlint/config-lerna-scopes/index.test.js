4aee2eef8ec71fb2f994390cdba4c16c34cbda0c
Armano <armano2@users.noreply.github.com>
2020-02-08 23:36:05 +0100

    test(config-lerna-scopes): improve tests for lerna 2 and split tests

--- @commitlint/config-lerna-scopes/index.test.js
+++ @commitlint/config-lerna-scopes/index.test.js
@@ -29,62 +29,69 @@ test('scope-enum is function', () => {
 });
 
 test('scope-enum does not throw for missing context', async () => {
-	const semverLt = jest.spyOn(semver, 'lt');
 	const {'scope-enum': fn} = config.rules;
 	await expect(fn()).resolves.toBeTruthy();
-	expect(semverLt).toHaveLastReturnedWith(false);
 });
 
 test('scope-enum has expected severity', async () => {
-	const semverLt = jest.spyOn(semver, 'lt');
 	const {'scope-enum': fn} = config.rules;
 	const [severity] = await fn();
 	expect(severity).toBe(2);
-	expect(semverLt).toHaveLastReturnedWith(false);
 });
 
 test('scope-enum has expected modifier', async () => {
-	const semverLt = jest.spyOn(semver, 'lt');
 	const {'scope-enum': fn} = config.rules;
 	const [, modifier] = await fn();
 	expect(modifier).toBe('always');
-	expect(semverLt).toHaveLastReturnedWith(false);
 });
 
 test('returns empty value for empty lerna repository', async () => {
-	const semverLt = jest.spyOn(semver, 'lt');
 	const {'scope-enum': fn} = config.rules;
 	const cwd = await lerna.bootstrap('empty', __dirname);
 	const [, , value] = await fn({cwd});
 	expect(value).toEqual([]);
-	expect(semverLt).toHaveLastReturnedWith(false);
 });
 
 test('returns expected value for basic lerna repository', async () => {
-	const semverLt = jest.spyOn(semver, 'lt');
 	const {'scope-enum': fn} = config.rules;
 	const cwd = await lerna.bootstrap('basic', __dirname);
 
 	const [, , value] = await fn({cwd});
 	expect(value).toEqual(['a', 'b']);
-	expect(semverLt).toHaveLastReturnedWith(false);
 });
 
 test('returns expected value for scoped lerna repository', async () => {
-	const semverLt = jest.spyOn(semver, 'lt');
 	const {'scope-enum': fn} = config.rules;
 	const cwd = await lerna.bootstrap('scoped', __dirname);
 
 	const [, , value] = await fn({cwd});
 	expect(value).toEqual(['a', 'b']);
-	expect(semverLt).toHaveLastReturnedWith(false);
 });
 
 test('works with lerna version < 3', async () => {
-	const semverLt = jest.spyOn(semver, 'lt');
 	const {'scope-enum': fn} = config.rules;
 	const cwd = await lerna.bootstrap('lerna-two', __dirname);
-	await expect(fn({cwd})).resolves.toBeTruthy();
+	const [, , value] = await fn({cwd});
+	expect(value).toEqual(['a2', 'b2']);
+});
+
+test('uses lerna version < 3 if installed', async () => {
+	const semverLt = jest.spyOn(semver, 'lt');
+	const cwd = await lerna.bootstrap('lerna-two', __dirname);
+	const packages = await config.utils.getPackages({cwd});
+
+	expect(packages).toEqual(['a2', 'b2']);
 	expect(semverLt).toHaveBeenLastCalledWith('2.11.0', '3.0.0');
 	expect(semverLt).toHaveLastReturnedWith(true);
+});
+
+test('uses lerna version >= 3 if installed', async () => {
+	const semverLt = jest.spyOn(semver, 'lt');
+	const cwd = await lerna.bootstrap('basic', __dirname);
+
+	const packages = await config.utils.getPackages({cwd});
+
+	expect(packages).toEqual(['a', 'b']);
+	expect(semverLt).toHaveBeenLastCalledWith('3.20.2', '3.0.0');
+	expect(semverLt).toHaveLastReturnedWith(false);
 });
