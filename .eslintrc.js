5d851ff31e0e2d5c7ba805d362dcb4699c5ca45b
Cedric van Putten <me@bycedric.com>
2021-02-24 11:15:30 +0100

    chore: reconfigure eslint for prettier config v8
    We need to do this for the breaking change mentioned in their changelog.
    
    See: https://github.com/prettier/eslint-config-prettier/blob/main/CHANGELOG.md#version-800-2021-02-21

--- .eslintrc.js
+++ .eslintrc.js
@@ -42,7 +42,7 @@ module.exports = {
 			extends: [
 				'plugin:@typescript-eslint/eslint-recommended',
 				'plugin:@typescript-eslint/recommended',
-				'prettier/@typescript-eslint',
+				'prettier',
 			],
 			rules: {
 				'@typescript-eslint/no-unused-vars': 'off',
