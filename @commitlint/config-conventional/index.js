688ad1e16650113374ead7dafc46ac774f57e759
Kristóf Poduszló <kripod@protonmail.com>
2021-03-28 12:06:01 +0200

    Allow any scope casing for Conventional Commits (#2529)
    * Allow any scope casing for Conventional Commits
    
    * Remove obsolete scope casing restriction test

--- @commitlint/config-conventional/index.js
+++ @commitlint/config-conventional/index.js
@@ -6,7 +6,6 @@ module.exports = {
 		'footer-leading-blank': [1, 'always'],
 		'footer-max-line-length': [2, 'always', 100],
 		'header-max-length': [2, 'always', 100],
-		'scope-case': [2, 'always', 'lower-case'],
 		'subject-case': [
 			2,
 			'never',
