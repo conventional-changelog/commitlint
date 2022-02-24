d6a5009cb5327bd8d3da8dd71766cf1907aa824b
Félix <legrelle.f@gmail.com>
2022-02-24 20:02:23 +0100

    test: improve type-empty test

--- @commitlint/rules/src/type-empty.test.ts
+++ @commitlint/rules/src/type-empty.test.ts
@@ -4,11 +4,15 @@ import {typeEmpty} from './type-empty';
 const messages = {
 	empty: '(scope):',
 	filled: 'type: subject',
+	subjectOnly: 'subject',
+	separator: ': subject',
 };
 
 const parsed = {
 	empty: parse(messages.empty),
 	filled: parse(messages.filled),
+	subjectOnly: parse(messages.subjectOnly),
+	separator: parse(messages.separator),
 };
 
 test('without type should succeed for empty keyword', async () => {
@@ -44,5 +48,41 @@ test('with type succeed for "never"', async () => {
 test('with type fail for "always"', async () => {
 	const [actual] = typeEmpty(await parsed.filled, 'always');
 	const expected = false;
+	expect(actual).toEqual(expected);
+});
+
+test('with only subject should succeed for empty keyword', async () => {
+	const [actual] = typeEmpty(await parsed.subjectOnly);
+	const expected = true;
+	expect(actual).toEqual(expected);
+});
+
+test('with only subject should fail for "never"', async () => {
+	const [actual] = typeEmpty(await parsed.subjectOnly, 'never');
+	const expected = false;
+	expect(actual).toEqual(expected);
+});
+
+test('with only subject should succeed for "always"', async () => {
+	const [actual] = typeEmpty(await parsed.subjectOnly, 'always');
+	const expected = true;
+	expect(actual).toEqual(expected);
+});
+
+test('without type but with a separator should succeed for empty keyword', async () => {
+	const [actual] = typeEmpty(await parsed.subjectOnly);
+	const expected = true;
+	expect(actual).toEqual(expected);
+});
+
+test('without type but with a separator should fail for "never"', async () => {
+	const [actual] = typeEmpty(await parsed.subjectOnly, 'never');
+	const expected = false;
+	expect(actual).toEqual(expected);
+});
+
+test('without type but with a separator should succeed for "always"', async () => {
+	const [actual] = typeEmpty(await parsed.subjectOnly, 'always');
+	const expected = true;
 	expect(actual).toEqual(expected);
 });
