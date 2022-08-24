784a4b36a0b5afdd32759754ec031994bbf0fdc5
morriswinkler <4302527+morriswinkler@users.noreply.github.com>
2022-08-24 09:11:00 +0200

    feat(commitlint): add additional git log args

--- @commitlint/read/src/read.ts
+++ @commitlint/read/src/read.ts
@@ -1,3 +1,5 @@
+import minimist from 'minimist';
+import type {GitOptions} from 'git-raw-commits';
 import {getHistoryCommits} from './get-history-commits';
 import {getEditCommit} from './get-edit-commit';
 
@@ -6,17 +8,27 @@ interface GetCommitMessageOptions {
 	from?: string;
 	to?: string;
 	edit?: boolean | string;
+	gitLogArgs?: string;
 }
 
 // Get commit messages
 export default async function getCommitMessages(
 	settings: GetCommitMessageOptions
 ): Promise<string[]> {
-	const {cwd, from, to, edit} = settings;
+	const {cwd, from, to, edit, gitLogArgs} = settings;
 
 	if (edit) {
 		return getEditCommit(cwd, edit);
 	}
 
-	return getHistoryCommits({from, to}, {cwd});
+	let gitOptions: GitOptions = {from, to};
+	if (gitLogArgs) {
+		gitOptions = {
+			...minimist(gitLogArgs.split(' ')),
+			from,
+			to,
+		};
+	}
+
+	return getHistoryCommits(gitOptions, {cwd});
 }
