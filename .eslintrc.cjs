f21489995f2db7c59f1ddde0ec5976d7aedb260d
JounQin <admin@1stg.me>
2024-02-05 09:38:48 +0800

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -65,7 +65,6 @@ module.exports = {
 			rules: {
 				'@typescript-eslint/no-explicit-any': 'off',
 				'@typescript-eslint/no-var-requires': 'off',
-
 				// disallow non-import statements appearing before import statements
 				'import/first': 'off',
 				'import/no-extraneous-dependencies': 'off',
