b27024a5ae509d1df9373ed712f2279d0bc39170
escapedcat <github@htmlcss.de>
2025-03-07 15:55:39 +0200

    Revert "fix: improve security validation regex in is-ignored function (#4258)" (#4314)
    This reverts commit 7403d6382cc2fb1f066a47d7229593eefe528561.

--- @commitlint/is-ignored/src/is-ignored.ts
+++ @commitlint/is-ignored/src/is-ignored.ts
@@ -1,6 +1,5 @@
 import {wildcards} from './defaults.js';
 import {IsIgnoredOptions} from '@commitlint/types';
-import {validateIgnoreFunction} from './validate-ignore-func.js';
 
 export default function isIgnored(
 	commit: string = '',
@@ -14,9 +13,6 @@ export default function isIgnored(
 		);
 	}
 
-	// Validate ignore functions
-	ignores.forEach(validateIgnoreFunction);
-
 	const invalids = ignores.filter((c) => typeof c !== 'function');
 
 	if (invalids.length > 0) {
@@ -28,13 +24,5 @@ export default function isIgnored(
 	}
 
 	const base = opts.defaults === false ? [] : wildcards;
-	return [...base, ...ignores].some((w) => {
-		const result = w(commit);
-		if (typeof result !== 'boolean') {
-			throw new Error(
-				`Ignore function must return a boolean, received ${typeof result}`
-			);
-		}
-		return result;
-	});
+	return [...base, ...ignores].some((w) => w(commit));
 }
