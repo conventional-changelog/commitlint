b7e404bc036dbd3cbdffa38e85c833d10e52d68b
Felipe Santos <felipecassiors@gmail.com>
2025-04-10 11:47:06 -0300

    perf(rules): optimize header-trim (#4363)
    * perf(rules): optimize header-trim
    
    * Simplify other lines
    
    * chore: fix formatting

--- @commitlint/rules/src/header-trim.ts
+++ @commitlint/rules/src/header-trim.ts
@@ -4,27 +4,19 @@ import { SyncRule } from "@commitlint/types";
 export const headerTrim: SyncRule = (parsed) => {
 	const { header } = parsed;
 
-	if (!header) {
-		return [true];
-	}
+	if (!header) return [true];
 
-	const startsWithWhiteSpace = header !== header.trimStart();
-	const endsWithWhiteSpace = header !== header.trimEnd();
+	const startsWithWhiteSpace = header.length > header.trimStart().length;
+	const endsWithWhiteSpace = header.length > header.trimEnd().length;
 
-	switch (true) {
-		case startsWithWhiteSpace && endsWithWhiteSpace:
-			return [
-				false,
-				message(["header", "must not be surrounded by whitespace"]),
-			];
+	if (startsWithWhiteSpace && endsWithWhiteSpace)
+		return [false, message(["header", "must not be surrounded by whitespace"])];
 
-		case startsWithWhiteSpace:
-			return [false, message(["header", "must not start with whitespace"])];
+	if (startsWithWhiteSpace)
+		return [false, message(["header", "must not start with whitespace"])];
 
-		case endsWithWhiteSpace:
-			return [false, message(["header", "must not end with whitespace"])];
+	if (endsWithWhiteSpace)
+		return [false, message(["header", "must not end with whitespace"])];
 
-		default:
-			return [true];
-	}
+	return [true];
 };
