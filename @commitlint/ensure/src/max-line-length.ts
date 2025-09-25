2f37a886be4b07bcd8e1126cc6a13256e7b6cc37
Adam Spiers <github@adamspiers.org>
2025-09-25 09:15:16 +0100

    feat(rules)!: make body-max-line-length ignore lines with URLs (#4486)
    BREAKING CHANGE: when lines contain URLs exceeding the maximum
    line length `body-max-line-length` will be ignored

--- @commitlint/ensure/src/max-line-length.ts
+++ @commitlint/ensure/src/max-line-length.ts
@@ -1,5 +1,13 @@
 import ensure from "./max-length.js";
 
+// Allow an exception for long lines which contain URLs.
+//
+// This is overly lenient, in order to avoid costly regexps which
+// have to worry about all the many edge cases of valid URLs.
+const URL_REGEX = /\bhttps?:\/\/\S+/;
+
 export default (value: string, max: number): boolean =>
 	typeof value === "string" &&
-	value.split(/\r?\n/).every((line) => ensure(line, max));
+	value
+		.split(/\r?\n/)
+		.every((line) => URL_REGEX.test(line) || ensure(line, max));
