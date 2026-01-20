11ae71427c3966b207951b981d23b849d9b7591d
hyperz111 <firzeroun11@gmail.com>
2026-01-20 09:40:12 +0700

    refactor: replace some lodash string methods with kasi and native

--- @commitlint/ensure/src/to-case.ts
+++ @commitlint/ensure/src/to-case.ts
@@ -1,28 +1,30 @@
 import { TargetCaseType } from "@commitlint/types";
-import camelCase from "lodash.camelcase";
-import kebabCase from "lodash.kebabcase";
-import snakeCase from "lodash.snakecase";
-import upperFirst from "lodash.upperfirst";
-import startCase from "lodash.startcase";
+import {
+	toCamelCase,
+	toKebabCase,
+	toSnakeCase,
+	toPascalCase,
+	toTitleCase,
+} from "kasi";
 
 export default function toCase(input: string, target: TargetCaseType): string {
 	switch (target) {
 		case "camel-case":
-			return camelCase(input);
+			return toCamelCase(input);
 		case "kebab-case":
-			return kebabCase(input);
+			return toKebabCase(input);
 		case "snake-case":
-			return snakeCase(input);
+			return toSnakeCase(input);
 		case "pascal-case":
-			return upperFirst(camelCase(input));
+			return toPascalCase(input);
 		case "start-case":
-			return startCase(input);
+			return toTitleCase(input);
 		case "upper-case":
 		case "uppercase":
 			return input.toUpperCase();
 		case "sentence-case":
 		case "sentencecase":
-			return upperFirst(input);
+			return input.charAt(0).toUpperCase() + input.slice(1);
 		case "lower-case":
 		case "lowercase":
 		case "lowerCase": // Backwards compat config-angular v4
