0afcd31b7a6fd5a1562cd6823040e9ea21214e7d
hyperz111 <firzeroun11@gmail.com>
2026-01-27 18:03:03 +0700

    refactor: replace find-up with escalade

--- @commitlint/top-level/src/index.ts
+++ @commitlint/top-level/src/index.ts
@@ -1,27 +1,15 @@
-import path from "node:path";
-import { findUp } from "find-up";
+import process from "node:process";
+import escalade from "escalade";
 
 export default toplevel;
 
 /**
  * Find the next git root
  */
-async function toplevel(cwd?: string) {
-	const found = await searchDotGit(cwd);
-
-	if (typeof found !== "string") {
-		return found;
-	}
-
-	return path.join(found, "..");
-}
-
-/**
- * Search .git, the '.git' can be a file(submodule), also can be a directory(normal)
- */
-async function searchDotGit(cwd?: string) {
-	const foundFile = await findUp(".git", { cwd, type: "file" });
-	const foundDir = await findUp(".git", { cwd, type: "directory" });
-
-	return foundFile || foundDir;
+function toplevel(cwd = process.cwd()) {
+	return escalade(cwd, (directory, files) => {
+		if (files.includes(".git")) {
+			return directory;
+		}
+	});
 }
