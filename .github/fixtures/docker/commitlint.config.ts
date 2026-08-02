6b1b9371381882d9c773cca554f94d3487e8afde
Guflly <145608489+Guflly@users.noreply.github.com>
2026-08-02 11:45:13 -0700

    test(container): strengthen TypeScript config check

--- .github/fixtures/docker/commitlint.config.ts
+++
@@ -1,8 +0,0 @@
-import { RuleConfigSeverity } from "@commitlint/types";
-import type { UserConfig } from "@commitlint/types";
-
-export default {
-	rules: {
-		"header-max-length": [RuleConfigSeverity.Error, "always", 100],
-	},
-} satisfies UserConfig;
